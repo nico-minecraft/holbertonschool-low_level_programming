4078a3f2c81f14b0cc80a487794dca79b81941df
Nico <nico.ls.asdf@gmail.com>
2026-06-26 19:30:02 -0600

    script 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - This prints the desired text using putss
+ * Return: should always be 0
+ */
 int main(void)
 	{
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
-	}
+	return (0);
+	} 
