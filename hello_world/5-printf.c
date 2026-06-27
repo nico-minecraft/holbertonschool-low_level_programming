561d94deb75e9b82ee6393f07f31ae5b42882f43
Nico <nico.ls.asdf@gmail.com>
2026-06-26 19:36:09 -0600

    Add script 5

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - This prints the desired text using putss
+ * main - This prints the desired text using printf
  * Return: should always be 0
  */
 int main(void)
 	{
-	print("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,");
 	return (0);
 	}
