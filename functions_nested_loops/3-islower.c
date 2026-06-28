4e2e30b36aa5ae50bf961cd3a61e73ec24ed7e8a
Nico <nico.ls.asdf@gmail.com>
2026-06-28 14:28:15 -0600

    Script 3

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -3,17 +3,11 @@
  * print_alphabet - Prints the whole alphabet
  * Return: should always be 0
  */
-int _islower(char c)
+int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z') {
-		_putchar(1);
+		return (1);
 	}
-	else {
-		_putchar(0);
-	}
-/*	if ((c >= 'A') && (c <= 'Z')) {
-		_putchar(1);
-	} */
 
 	return 0;
 }
