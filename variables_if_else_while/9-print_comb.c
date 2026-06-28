d834c554d4db3b3ac71da345176530363c14b266
Nico <nico.ls.asdf@gmail.com>
2026-06-28 00:52:25 -0600

    Script 9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Prints all single digit numbers as strings
+ * Return: should always be 0
+ */
+
+int main(void)
+{
+	char n = '0';
+	char o = ',';
+	char t = ' ';
+
+	while (n <= '9')
+	{
+		putchar(n);
+		if (n < '9')
+		{
+			putchar(o);
+			putchar(t);
+		}
+		n++;
+	}
+
+	putchar('\n');
+	return (0);
+}
