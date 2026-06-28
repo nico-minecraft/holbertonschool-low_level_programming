f2daa4a567374e16691f03fe7e5a422f5f14435b
Nico <nico.ls.asdf@gmail.com>
2026-06-28 13:42:06 -0600

    Script 2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Prints the whole alphabet ten times
+ * Return: should always be 0
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		char a = 'a';
+
+		while (a <= 'z')
+		{
+			_putchar(a);
+			a++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
