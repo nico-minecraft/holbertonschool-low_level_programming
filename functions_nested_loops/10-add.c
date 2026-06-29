f565765686ca19b17aab3ef0ea897ebb904683eb
Nico <nico.ls.asdf@gmail.com>
2026-06-28 20:29:47 -0600

    Script 10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two numbers together and returns the output
+ * @a: first number
+ * @b: second number
+ * Return: sum of a + b
+ */
+int add(int a, int b)
+
+{
+	int result = a + b;
+
+	return (result);
+
+}
