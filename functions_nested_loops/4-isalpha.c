42b2b9caa0cad6f98c69a3739b435c74b67de8f3
Nico <nico.ls.asdf@gmail.com>
2026-06-28 20:24:39 -0600

    Script 4

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _isalpha - Determines if the character is alphanumeric
- * Return: 1 if it is lowercase, 0 if it is not
+ * Return: 1 if it is alphanumeric, 0 if it is not
  * @c: is an int variable for the letter to be checked
  */
 int _isalpha(int c)
