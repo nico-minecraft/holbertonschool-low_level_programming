838fec85a55759e260b1793ce8834d8561870602
Nico <nico.ls.asdf@gmail.com>
2026-06-28 19:35:21 -0600

    Script 5

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -8,16 +8,19 @@ int print_sign(int n)
 {
 	if (n >= 1)
 	{
+		_putchar('+');
 		return (1);
 	}
 
 	if (n == 0)
 	{
+		_putchar('0');
 		return (0);
 	}
 
 	if (n <= -1)
 	{
+		_putchar('-');
 		return (-1);
 	}
 
