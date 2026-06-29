c913ceefa962afae81b41c304cf0ecb808cdab33
Nico <nico.ls.asdf@gmail.com>
2026-06-28 23:48:19 -0600

    Script 9

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - prints numbers 1 to 100, replacing multiples of 3 with "Fizz",
+ *        multiples of 5 with "Buzz", and multiples of both with "FizzBuzz"
+ * Return: Should always be 0.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", i);
+
+		if (i != 100)
+			printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
