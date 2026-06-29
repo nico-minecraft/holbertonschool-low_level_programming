08307306bb416ba0fdd476a94a0d7eafa6141b9d
Nico <nico.ls.asdf@gmail.com>
2026-06-28 19:31:48 -0600

    Script 5

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_sign - Determines if the int is positive, negative, or 0
+ * Return: -1 for negative, 0 for 0, +1 for positive
+ * @n: is an int variable for the number to be checked
+ */
+int print_sign(int n)
+{
+	if (n >= 1)
+	{
+		return (1);
+	}
+
+	if (n == 0)
+	{
+		return (0);
+	}
+
+	if (n <= -1)
+	{
+		return (-1);
+	}
+
+	return (0);
+}
