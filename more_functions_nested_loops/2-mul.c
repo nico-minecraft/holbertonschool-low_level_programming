e64bf45f8deea332669c9b6e53c42626c1fda0e1
Nico <nico.ls.asdf@gmail.com>
2026-06-28 23:05:46 -0600

    Script 2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - Multiplies two numbers together and returns the output
+ * @a: first number
+ * @b: second number
+ * Return: Product of a * b
+ */
+int mul(int a, int b)
+
+{
+	int result = a * b;
+
+	return (result);
+
+}
