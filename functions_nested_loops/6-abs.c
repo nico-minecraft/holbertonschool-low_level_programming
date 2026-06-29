b503b2fcfc7bd6d3b90fb1ef7062c90621dda21e
Nico <nico.ls.asdf@gmail.com>
2026-06-28 19:44:25 -0600

    Script 6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _abs - Print the absolute value of input
+ * Return: absolute value of input x
+ * @x: the number to be absolutely evaluated
+ */
+int _abs(int x)
+{
+	if (x < 0)
+		return (-x);
+	return (x);
+}
