5a352f9e5c7cb85e65e0e332dbd8b4536d51a3c0
Nico <nico.ls.asdf@gmail.com>
2026-06-28 23:20:09 -0600

    Script 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_most_numbers - Prints all single digit numbers except for 2 and 4
+ * Return: should always be 0
+ */
+void print_most_numbers(void)
+{
+	char n = '0';
+
+	while (n <= '9')
+	{
+		if (n != '2' && n != '4')
+		{
+			_putchar(n);
+		}
+		n++;
+	}
+	_putchar('\n');
+}
