5fdc72f1cf6b63bae92d2e41b342a0a298bd52fb
Nico <nico.ls.asdf@gmail.com>
2026-06-28 20:48:21 -0600

    Script 11

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - Prints the number, approaching closer to 98,
+ *               be it growing or shrinking
+ * @n: number to reach 98
+ */
+
+void print_to_98(int n)
+{
+
+	do {
+		printf("%d, ", n);
+		if (n < 98)
+		{
+			n++;
+		}
+		else if (n > 98)
+		{
+			n--;
+		}
+	} while (n != 98);
+}
