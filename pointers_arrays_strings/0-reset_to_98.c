30cc2d94696adaf35232020171770b6a9c047ecd
Nico <nico.ls.asdf@gmail.com>
2026-07-04 17:20:11 -0600

    Script 0

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_numbers - Prints all single digit numbers as strings
+ * Return: should always be 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
