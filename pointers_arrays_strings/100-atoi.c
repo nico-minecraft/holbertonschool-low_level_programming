95b946cebe5bab1612ffc994fabb1b0096ccf7b9
Nico <nico.ls.asdf@gmail.com>
2026-07-05 16:33:15 -0600

    Script 100

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -5,45 +5,43 @@
  */
 int _atoi(char *s)
 {
-    int i;
-    int sign;
-    int started;
-    unsigned int num;
+int i;
+int sign;
+int started;
+unsigned int num;
 
-    i = 0;
-    sign = 1;
-    started = 0;
-    num = 0;
+i = 0;
+sign = 1;
+started = 0;
+num = 0;
 
-    while (s[i] != '\0')
-    {
-        if (s[i] == '-')
-        {
-            if (started)
-                break;
-            sign *= -1;
-        }
-        else if (s[i] == '+')
-        {
-            if (started)
-                break;
-        }
-        else if (s[i] >= '0' && s[i] <= '9')
-        {
-            started = 1;
-            num = (num * 10) + (unsigned int)(s[i] - '0');
-        }
-        else
-        {
-            if (started)
-                break;
-            sign = 1;
-        }
-        i++;
-    }
-
-    if (sign == -1)
-        num = -num;
-
-    return ((int)num);
+while (s[i] != '\0')
+{
+	if (s[i] == '-')
+	{
+		if (started)
+			break;
+		sign *= -1;
+	}
+	else if (s[i] == '+')
+	{
+		if (started)
+			break;
+	}
+	else if (s[i] >= '0' && s[i] <= '9')
+	{
+		started = 1;
+		num = (num * 10) + (unsigned int)(s[i] - '0');
+	}
+	else
+	{
+		if (started)
+			break;
+		sign = 1;
+	}
+	i++;
+}
+if (sign == -1)
+	num = -num;
+return ((int)num);
 }
