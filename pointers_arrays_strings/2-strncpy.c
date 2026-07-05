46e9e82c4f845deea36062e0c8cb46df6dfe4459
Nico <nico.ls.asdf@gmail.com>
2026-07-05 17:48:54 -0600

    Script 2

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+/**
+ * _strncpy - copies a string, behaving exactly like the standard strncpy
+ * @dest: destination buffer
+ * @src: source string to copy
+ * @n: maximum number of bytes to write to dest
+ * Return: a pointer to dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+    int i;
+
+    i = 0;
+    while (i < n && src[i] != '\0')
+    {
+        dest[i] = src[i];
+        i++;
+    }
+    while (i < n)
+    {
+        dest[i] = '\0';
+        i++;
+    }
+
+    return (dest);
+}
