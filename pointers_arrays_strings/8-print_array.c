95e6ee1688c556a2fc146a39c47e75dc0829da03
Nico <nico.ls.asdf@gmail.com>
2026-07-05 16:00:09 -0600

    Script 8

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -8,13 +8,13 @@
  */
 void print_array(int *a, int n)
 {
-    int i;
+	int i;
 
-    for (i = 0; i < n; i++)
-    {
-        if (i > 0)
-            printf(", ");
-        printf("%d", a[i]);
-    }
-    printf("\n");
+	for (i = 0; i < n; i++)
+	{
+		if (i > 0)
+			printf(", ");
+		printf("%d", a[i]);
+	}
+	printf("\n");
 }
