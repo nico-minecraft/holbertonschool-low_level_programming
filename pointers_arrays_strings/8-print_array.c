0263a572aba842bfb20c1027a41d1923ef291c39
Nico <nico.ls.asdf@gmail.com>
2026-07-05 15:59:19 -0600

    Script 8

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * print_array - prints n elements of an array of integers, followed
+ * by a newline
+ * @a: pointer to the array
+ * @n: number of elements to print
+ */
+void print_array(int *a, int n)
+{
+    int i;
+
+    for (i = 0; i < n; i++)
+    {
+        if (i > 0)
+            printf(", ");
+        printf("%d", a[i]);
+    }
+    printf("\n");
+}
