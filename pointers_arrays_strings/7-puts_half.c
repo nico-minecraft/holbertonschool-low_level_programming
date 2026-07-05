0ade4852d8a8ce9f803743c2a25b371b30edcd52
Nico <nico.ls.asdf@gmail.com>
2026-07-04 22:12:19 -0600

    Script 7

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,17 +1,23 @@
 #include "main.h"
+
 /**
  * puts_half - prints the second half of a string, followed by a newline
  * @str: the string to print
  */
 void puts_half(char *str)
 {
-	int len, start, i;
+	int len;
+	int start;
+	int i;
 
 	len = 0;
 	while (str[len] != '\0')
 		len++;
 
-	start = len / 2;
+	if (len % 2 == 0)
+		start = len / 2;
+	else
+		start = (len - ((len + 1) / 2));
 
 	i = start;
 	while (str[i] != '\0')
