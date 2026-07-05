0080c37ade404680bbced4d94213f267ac20d060
Nico <nico.ls.asdf@gmail.com>
2026-07-04 21:35:21 -0600

    Script 5

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+/**
+ * rev_string - reverses a string in place
+ * @s: the string to reverse
+ *
+ * Return: nothing
+ */
+void rev_string(char *s)
+{
+    int start, end;
+    char temp;
+
+    start = 0;
+    end = 0;
+
+    while (s[end] != '\0')
+        end++;
+    end--;
+
+    while (start < end)
+    {
+        temp = s[start];
+        s[start] = s[end];
+        s[end] = temp;
+        start++;
+        end--;
+    }
+}
