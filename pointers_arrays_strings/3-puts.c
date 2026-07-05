6a601311500567d546e0abbb6df3154fac0b712a
Nico <nico.ls.asdf@gmail.com>
2026-07-04 21:09:24 -0600

    Script 3

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -6,13 +6,14 @@
  */
 void _puts(char *str)
 {
-    int i;
+	int i;
 
-    i = 0;
-    while (str[i] != '\0')
-    {
-        write(1, &str[i], 1);
-        i++;
-    }
-    write(1, "\n", 1);
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+	write(1, "\n", 1);
 }
