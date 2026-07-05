0195cd100f599df2c047ef94088126a6cb485d6a
Nico <nico.ls.asdf@gmail.com>
2026-07-05 16:39:01 -0600

    Script 100

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,47 +1,46 @@
 /**
  * _atoi - converts a string to an integer
  * @s: the string to convert
+ *
  * Return: the converted integer, or 0 if no digits are found
  */
 int _atoi(char *s)
 {
-int i;
-int sign;
-int started;
-unsigned int num;
+	int i;
+	int sign;
+	int started;
+	unsigned int num;
 
-i = 0;
-sign = 1;
-started = 0;
-num = 0;
+	i = 0;
+	sign = 1;
+	started = 0;
+	num = 0;
 
-while (s[i] != '\0')
-{
-	if (s[i] == '-')
+	while (s[i] != '\0')
 	{
-		if (started)
+	if (started)
+	{
+		if (s[i] >= '0' && s[i] <= '9')
+			num = (num * 10) + (unsigned int)(s[i] - '0');
+		else
 			break;
-		sign *= -1;
-	}
-	else if (s[i] == '+')
-	{
-		if (started)
-			break;
-	}
-	else if (s[i] >= '0' && s[i] <= '9')
-	{
-		started = 1;
-		num = (num * 10) + (unsigned int)(s[i] - '0');
 	}
 	else
 	{
-		if (started)
-			break;
-		sign = 1;
+		if (s[i] == '-')
+			sign *= -1;
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			started = 1;
+			num = (unsigned int)(s[i] - '0');
+		}
+		/* '+' and any other character before the number: ignored */
 	}
 	i++;
-}
-if (sign == -1)
-	num = -num;
-return ((int)num);
+	}
+
+	if (sign == -1)
+		num = -num;
+
+	return ((int)num);
 }
