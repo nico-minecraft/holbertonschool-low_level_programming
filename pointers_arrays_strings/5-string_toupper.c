dbff31d99882a0fde39b8da483786a2540c66b17
Nico <nico.ls.asdf@gmail.com>
2026-07-05 19:42:01 -0600

    Script 5

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ * @s: the string to modify
+ *
+ * Return: a pointer to the modified string
+ */
+char *string_toupper(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] = s[i] - 'a' + 'A';
+		i++;
+	}
+
+	return (s);
+}
