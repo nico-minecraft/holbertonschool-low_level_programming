0273eb9050949a291ec3d747b1219b35cf5e6cb6
Nico <nico.ls.asdf@gmail.com>
2026-07-05 19:32:53 -0600

    Script 4

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -7,17 +7,17 @@
  */
 void reverse_array(int *a, int n)
 {
-    int start, end, temp;
+	int start, end, temp;
 
-    start = 0;
-    end = n - 1;
+	start = 0;
+	end = n - 1;
 
-    while (start < end)
-    {
-        temp = a[start];
-        a[start] = a[end];
-        a[end] = temp;
-        start++;
-        end--;
-    }
+	while (start < end)
+	{
+		temp = a[start];
+		a[start] = a[end];
+		a[end] = temp;
+		start++;
+		end--;
+	}
 }
