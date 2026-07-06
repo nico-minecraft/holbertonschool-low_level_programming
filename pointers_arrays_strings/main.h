ad243488af087cf98801ae15c670f85b0eea8e0b
Nico <nico.ls.asdf@gmail.com>
2026-07-05 21:47:04 -0600

    Final header

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -2,11 +2,10 @@
 #define MAIN_H
 
 int _putchar(char c);
-void reset_to_98(int *n);
-void swap_int(int *a, int *b);
-int _strlen(char *s);
-void _puts(char *str);
-void print_rev(char *s);
-void rev_string(char *s);
+char *_memset(char *s, char b, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+void print_chessboard(char (*a)[8]);
 
 #endif
