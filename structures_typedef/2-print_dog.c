1778704e4d07e6575cfa6f37df4d04ee9040ce47
Nico <nico.ls.asdf@gmail.com>
2026-07-12 17:27:04 -0600

    Script 2

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,12 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ * print_dog - Prints the dog
+ * @d: dog struct
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+}
