cc386bc192b257f8eb9553416220fe32b3922ae8
Nico <nico.ls.asdf@gmail.com>
2026-07-12 16:15:17 -0600

    Script 1

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+/**
+ * init_dog - Initialized the dog
+ * @d: dog
+ * @name: Dog name
+ * @age: Dog age
+ * @owner: Dog owner
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
