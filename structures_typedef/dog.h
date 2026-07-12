b38ff2371d2c62688696d0466b158689072500e8
Nico <nico.ls.asdf@gmail.com>
2026-07-12 16:04:11 -0600

    Add dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * struct dog - Dog structure
+ * @name: Dog name
+ * @age: Dog age
+ * @owner: Dog owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
