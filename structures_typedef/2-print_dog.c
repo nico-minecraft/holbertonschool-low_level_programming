e8deb6af6e8eeba303a94c7d8ffc71ef8832b861
Nico <nico.ls.asdf@gmail.com>
2026-07-12 17:46:16 -0600

    Script 2

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -8,5 +8,8 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
-	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+
+	printf("Name: %s\n", d->name != NULL ? d->name : "(nil)");
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", d->owner != NULL ? d->owner : "(nil)");
 }
