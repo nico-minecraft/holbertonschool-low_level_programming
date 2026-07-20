a94482fa31cedb187e263c541c17cf8c159a5aef
Nico <nico.ls.asdf@gmail.com>
2026-07-19 21:27:37 -0600

    Script 2

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,22 @@
+/**
+ * int_index - Searches for an integer
+ * Return: Returns index, if invalid, returns -1
+ * @array: Array
+ * @size: Size of the array
+ * @cmp: Pointer to the function to be used to compare values
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i;
+
+if (size <= 0)
+return (-1);
+
+for (i = 0; i < size; i++)
+{
+if (cmp(array[i]) != 0)
+return (i);
+}
+
+return (-1);
+}
