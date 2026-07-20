f19ad7138fe14f3b5dd9c95d8b4b60904404edd3
Nico <nico.ls.asdf@gmail.com>
2026-07-19 21:42:42 -0600

    Script 3

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,28 @@
+#include "3-calc.h"
+
+/**
+ * get_op_func - selects the function associated to an operator
+ * @s: the operator string
+ * Return: pointer to the matching function, or NULL if none matches
+ */
+int (*get_op_func(char *s))(int, int)
+{
+op_t ops[] = {
+{"+", op_add},
+{"-", op_sub},
+{"*", op_mul},
+{"/", op_div},
+{"%", op_mod},
+{NULL, NULL}
+};
+int i;
+
+i = 0;
+while (ops[i].op != NULL)
+{
+if (strcmp(ops[i].op, s) == 0)
+return (ops[i].f);
+i++;
+}
+return (NULL);
+}
