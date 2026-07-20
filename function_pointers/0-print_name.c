811088d350cd7d9fe2315812aff7055554accafd
Nico <nico.ls.asdf@gmail.com>
2026-07-19 21:08:06 -0600

    Script 0

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-
+/**
+ * print_name - Prints the name provided
+ * @name: Name to print
+ * @f: Pointer function
+ */
 void print_name(char *name, void (*f)(char *))
 {
-    f(name);
+f(name);
 }
