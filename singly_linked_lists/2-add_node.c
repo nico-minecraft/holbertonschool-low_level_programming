ee0970c9b905ece079074ca34327410d0974dfab
Nico <nico.ls.asdf@gmail.com>
2026-07-26 23:42:36 -0600

    Script 2

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,5 +1,7 @@
+#include <stdlib.h>
+#include <string.h>
 #include "lists.h"
-#include <stdio.h>
+
 /**
  * add_node - adds a new node at the beginning of a list_t list
  * @head: pointer to a pointer to the head of the list
