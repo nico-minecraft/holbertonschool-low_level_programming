a654270f6bd11a22e72de2dbf6e286af31969f42
Nico <nico.ls.asdf@gmail.com>
2026-08-09 17:41:53 -0600

    Add script 3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -37,14 +37,12 @@ int main(int ac, char **av)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
 	fd_from = open(av[1], O_RDONLY);
 	if (fd_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
-
 	fd_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC,
 		S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 	if (fd_to == -1)
@@ -52,7 +50,6 @@ int main(int ac, char **av)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 		exit(99);
 	}
-
 	r = read(fd_from, buf, BUF_SIZE);
 	while (r > 0)
 	{
@@ -69,9 +66,7 @@ int main(int ac, char **av)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
-
 	close_fd(fd_from);
 	close_fd(fd_to);
-
 	return (0);
 }
