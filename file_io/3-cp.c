061fb0523599a341fb6b9cf7eff7ec4d018a70d4
Nico <nico.ls.asdf@gmail.com>
2026-08-09 17:39:45 -0600

    Add script 3

---
+++ file_io/3-cp.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#define BUF_SIZE 1024
+
+/**
+ * close_fd - closes a file descriptor and exits with code 100 on failure
+ * @fd: the file descriptor to close
+ */
+void close_fd(int fd)
+{
+	if (close(fd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+
+/**
+ * main - copies the content of a file to another file
+ * @ac: argument count
+ * @av: argument vector
+ *
+ * Return: Always 0.
+ */
+int main(int ac, char **av)
+{
+	int fd_from, fd_to, r, w;
+	char buf[BUF_SIZE];
+
+	if (ac != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	fd_from = open(av[1], O_RDONLY);
+	if (fd_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+
+	fd_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC,
+		S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	if (fd_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		exit(99);
+	}
+
+	r = read(fd_from, buf, BUF_SIZE);
+	while (r > 0)
+	{
+		w = write(fd_to, buf, r);
+		if (w == -1 || w != r)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+			exit(99);
+		}
+		r = read(fd_from, buf, BUF_SIZE);
+	}
+	if (r == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+
+	close_fd(fd_from);
+	close_fd(fd_to);
+
+	return (0);
+}
