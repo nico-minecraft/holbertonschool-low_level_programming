579ae14f37267244ee1553f22a9386bdea4c0bdc
Nico <nico.ls.asdf@gmail.com>
2026-08-09 16:32:04 -0600

    Add script 4

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - returns the value of x raised to the power of y,
+ * using recursion
+ * @x: the base
+ * @y: the power
+ *
+ * Return: x raised to the power of y, or -1 if y is lower than 0
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+
+	if (y == 0)
+		return (1);
+
+	return (x * _pow_recursion(x, y - 1));
+}
